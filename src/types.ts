83d9d73e86c9f4911e06836ac1711f8660320c90
yosiasnawipa5-code <yosiasnawipa5@gmail.com>
2026-05-10 00:40:42 +0700

    Define types and interfaces for Todo application
    interface vs type

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,30 @@
 // TODO: Buat tipe untuk status To-Do (active/done)
 
 // TODO: Buat tipe untuk fungsi-fungsi yang akan digunakan
+
+//status: "active"✅ | status: "done" ✅ | status "pending" ❌
+export type TodoStatus = "active" | "done";
+
+//✅ interface untuk satu item To-Do
+export interface Todo {
+  id: number;      //Angka unik pengenal to-do
+  title: string;    //Judul /deskripsi tugas
+  status: TodoStatus;  //Hanya boleh "active" atau "done"
+  createdAt: string;    //Tanggal dibuat (format ISO string)
+}
+
+//✅ Type alias untuk array of Todo
+export type TodoList = Todo[];
+
+//✅ Interface untuk operasi yang bisa dilakukan user
+export interface TodoOperation {
+  addTodo(title: string): void;
+  completeTodo(id: number): void;
+  deleteTodo(id: number): void;
+  listTodo(): void;
+}
+
+//✅ Type untuk hasil operasi yang bisa sukses atau gagal
+export type OperationResult =
+  | { success: true; message: string }
+  | { success: false; error: string }
